342ee7061e0f06857ee78edc77d6933737040282
Anfe-ion <andres.moncada.c@gmail.com>
2024-06-21 10:52:06 -0500

    Basics done

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from "vue";
+import { ref, computed } from "vue";
 
 const name = 'Dinamic Vue'
 const style = 'font-size: 100px; font-weight: bold; color: pink'
@@ -27,104 +27,144 @@ const arrayObjetos =
       stock: 20,
     }
   ]
-  const arrayObjetosUnico =
+const arrayObjetosUnico =
 {
   name: "Manzana",
   price: "$1.00",
   description: "Una manzana",
 }
-  //Methods
-  const hadleClick = (message) => {
-    console.log(message)
+//Methods
+const hadleClick = (message) => {
+  console.log(message)
+}
+
+const counter = ref(0)
+const arrayFavs = ref([]);
+
+const increment = () => counter.value++;
+
+const decrement = () => counter.value--;
+
+const reset = () => counter.value = 0;
+
+const add = () => {
+  arrayFavs.value.push(counter.value)
+}
+
+const blockButton = computed(() => {
+  if (arrayFavs.value.includes(counter.value)) {
+    return true
   }
+})
 
-  const counter = ref(0)
+const classCounter = computed(() => {
+  if (counter.value == 0) {
+    return 'zero'
+  } if (counter.value > 0) {
+    return 'positive'
+  } else {
+    return 'negative'
+  }
+})
 
-  const increment = () => counter.value ++;
 
-  const decrement = () => counter.value --;
-
-  const reset = () => counter.value = 0;
-  
 </script>
 
 <template>
-  <h1>{{ name.toUpperCase() }}</h1>
 
-  <p :style="style">Perrito</p>
+  <div>
+    <h1>{{ name.toUpperCase() }}</h1>
 
-  <p :style="`color: ${arrayColors[1]}`">Soy {{ arrayColors[1] }}</p>
+    <!-- <p :style="style">Perrito</p> -->
 
-  <p :style="{ color: activo ? 'green' : 'red' }">
-    {{ activo ? "I'm Active" : "I'm not active" }}
-  </p>
+    <p :style="`color: ${arrayColors[1]}`">Soy {{ arrayColors[1] }}</p>
 
-  <p v-if="activo">I'm Active</p>
-  <p v-else>I'm not Active</p>
+    <p :style="{ color: activo ? 'green' : 'red' }">
+      {{ activo ? "I'm Active" : "I'm not active" }}
+    </p>
 
-  {{ arrayFrutas }}
+    <p v-if="activo">I'm Active</p>
+    <p v-else>I'm not Active</p>
 
-  <p v-for="(fruta, index) in arrayFrutas" v-bind:key="index">
-    {{ index + 1 }} - {{ fruta }}
-  </p>
+    {{ arrayFrutas }}
 
-  <p v-for="objeto in arrayObjetos" key="fruta.name">
-    {{ objeto }}
-  </p>
+    <p v-for="(fruta, index) in arrayFrutas" v-bind:key="index">
+      {{ index + 1 }} - {{ fruta }}
+    </p>
 
-  <p v-for="objeto in arrayObjetos" key="fruta.name">
-    {{ objeto.name }}
-  </p>
+    <p v-for="objeto in arrayObjetos" key="fruta.name">
+      {{ objeto }}
+    </p>
 
-  <p v-for="objeto in arrayObjetos" key="fruta.name">
-    {{ objeto.price }}
-  </p>
+    <p v-for="objeto in arrayObjetos" key="fruta.name">
+      {{ objeto.name }}
+    </p>
 
-  <p v-for="objeto in arrayObjetos" key="fruta.name">
-    {{ objeto.description }}
-  </p>
+    <p v-for="objeto in arrayObjetos" key="fruta.name">
+      {{ objeto.price }}
+    </p>
 
-  <p v-for="(objeto, index) in arrayObjetos" key="name">
-    {{ index + 1 }}. {{ objeto.name }} que cuesta {{ objeto.price }} y es {{ objeto.description }}
-  </p>
+    <p v-for="objeto in arrayObjetos" key="fruta.name">
+      {{ objeto.description }}
+    </p>
 
-  <hr>
+    <p v-for="(objeto, index) in arrayObjetos" key="name">
+      {{ index + 1 }}. {{ objeto.name }} que cuesta {{ objeto.price }} y es {{ objeto.description }}
+    </p>
 
-  <ul>
-    <li v-for="(value, propiedad, index) in arrayObjetosUnico" :key="value.arrayObjetosUnico">
-      {{ index + 1 }}. {{ propiedad }}: {{ value }}
-    </li>
-  </ul>
+    <hr>
+    <ul>
+      <li v-for="(value, propiedad, index) in arrayObjetosUnico" :key="value.arrayObjetosUnico">
+        {{ index + 1 }}. {{ propiedad }}: {{ value }}
+      </li>
+    </ul>
 
-  <template v-for="item in arrayObjetos" :key="name.arrayObjetos">
-    <li v-if="item.stock > 0">{{ item.name }}</li>
-  </template>
+    <template v-for="item in arrayObjetos" :key="name.arrayObjetos">
+      <li v-if="item.stock > 0">{{ item.name }}</li>
+    </template>
+  </div>
 
   <!-- Events -->
-   <div>
-     <button v-on:click="hadleClick('Active 1')">Active me</button>
-     <button @click="hadleClick('Active 2')">Active me</button>
-     <br>
-     <button @click.left="hadleClick('Left Text')">Active me left</button>
-     <button @click.middle.prevent ="hadleClick('Middle Text')">Active me Middle</button>
-     <button @click.right.prevent="hadleClick('Right Text')">Active me right</button>
-   </div>
+  <div>
+    <button v-on:click="hadleClick('Active 1')">Active me</button>
+    <button @click="hadleClick('Active 2')">Active me</button>
+    <br>
+    <button @click.left="hadleClick('Left Text')">Active me left</button>
+    <button @click.middle.prevent="hadleClick('Middle Text')">Active me Middle</button>
+    <button @click.right.prevent="hadleClick('Right Text')">Active me right</button>
+  </div>
 
-   <div>
+  <div>
     <button @click="increment">Aumentar</button>
     <button @click="decrement">Decrementar</button>
     <button @click="reset">Reset</button>
-    <p :style="{color: counter > 0 ? 'green' : (counter == 0) ? 'black' : 'red'}"> {{ counter }}</p>
-   </div>
-
-
-
+    <button @click="add" :disabled="blockButton">Add to favorites</button>
+    <p :class="classCounter"> {{ counter }}</p>
+    <ul v-for="numeros in arrayFavs">
+      <li>{{ numeros }}</li>
+    </ul>
+  </div>
 
 </template>
 
 <style>
 h1 {
   color: blueviolet;
+  font-weight: bold;
+}
+
+.zero {
+  color: black;
+  font-weight: bold;
+}
+
+.positive {
+  color: greenyellow;
+  font-weight: bold;
+}
+
+.negative {
+  color: palevioletred;
   font-weight: bold;
 }
 </style>
